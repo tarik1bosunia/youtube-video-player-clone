61fc3d769948599492623ba33a764c4b39f3cf8b
md tarik bosunia <bosuniamdtarik005@gmail.com>
2023-12-07 11:31:08 +0600

    different screen mode working properly , but some design is not working properly with theater mode

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const playPauseBtn = document.querySelector('.play-pause-btn')
 const video = document.querySelector('video')
 const videoContainer = document.querySelector('.video-container')
+const theaterBtn = document.querySelector('.theater-btn')
+const miniPlayerBtn = document.querySelector('.mini-player-btn')
+const fullScreenBtn = document.querySelector('.full-screen-btn')
 
 
 
@@ -10,10 +13,59 @@ document.addEventListener('keydown', e =>{
         case " ":
         case "k":
            togglePlay()
-           break        
+           break
+        case "f":
+            toggleFullScreenMode()
+            break      
+            
+        case "t":
+            toggleTheaterMode()
+            break 
+            
+        case "i":
+            toggleMiniPlayerMode()    
+            break            
     }
 })
 
+// View Modes
+theaterBtn.addEventListener('click', toggleTheaterMode)
+fullScreenBtn.addEventListener('click', toggleFullScreenMode)
+miniPlayerBtn.addEventListener('click', toggleMiniPlayerMode)
+
+function toggleTheaterMode(){
+    videoContainer.classList.toggle('theater')
+}
+
+function toggleFullScreenMode(){
+    if(document.fullscreenElement == null){
+        videoContainer.requestFullscreen()
+
+    }else{
+        document.exitFullscreen()
+        
+    } 
+}
+document.addEventListener('fullscreenchange', (e)=> {
+    videoContainer.classList.toggle('full-screen', document.fullscreenElement)
+})
+
+function toggleMiniPlayerMode(){
+    if(videoContainer.classList.contains('mini-player')){
+        document.exitPictureInPicture()
+
+    }else{
+        video.requestPictureInPicture()
+    } 
+}
+
+video.addEventListener('enterpictureinpicture', ()=>{
+    videoContainer.classList.add('mini-player')
+})
+
+video.addEventListener('leavepictureinpicture', ()=>{
+    videoContainer.classList.remove('mini-player')
+})
 
 //  play/pause
 function togglePlay(e){
